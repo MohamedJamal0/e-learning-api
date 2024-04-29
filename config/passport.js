5a635dd9001ab4ce08c5bdf303ac5020b9efd934
mohamedjamal0 <mohamedjamal9810@gmail.com>
2024-04-29 07:29:55 +0300

    feat: Update Google OAuth callback URL for production environment

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,18 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+  const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-module.exports = (passport) => {
-  passport.use(
-    'google',
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/v1/auth/googleRedirect',
-      },
-      async function (accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-      }
-    )
-  );
-};
+  module.exports = (passport) => {
+    passport.use(
+      'google',
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL:
+            'https://e-learning-api-ruby.vercel.app/v1/auth/googleRedirect',
+        },
+        async function (accessToken, refreshToken, profile, done) {
+          return done(null, profile);
+        }
+      )
+    );
+  };
