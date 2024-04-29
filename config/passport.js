65ae8e1ba046f4155b451d6433c3c1f82772bd6c
mohamedjamal0 <mohamedjamal9810@gmail.com>
2024-04-30 02:31:19 +0300

    chore : update google auth callback url

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,8 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        callbackURL:
+          'https://e-learning-api-o2t2.onrender.com/v1/auth/googleRedirect',
       },
       async function (accessToken, refreshToken, profile, done) {
         return done(null, profile);
