cb6302ad55e2d9142ba2bae8bec4298786c5d7d9
mohamedjamal0 <mohamedjamal9810@gmail.com>
2024-05-02 18:50:33 +0300

    chore : update the callback url

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = (passport) => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          'https://e-learning-api-o2t2.onrender.com/v1/auth/googleRedirect',
+          'https://e-learning-api-ruby.vercel.app/v1/auth/googleRedirect',
       },
       async function (accessToken, refreshToken, profile, done) {
         return done(null, profile);
